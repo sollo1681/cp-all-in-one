6315e8efb7a0dad21ca87b7430d464c5c11339ac
sollo <userlogin@email.cz>
2022-06-23 23:55:07 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+<<<<<<< Updated upstream
 docker-compose -f $INPUT_TYPE/docker-compose.yml up -d $INPUT_SERVICE
+=======
+cp $INPUT_TYPE/docker-compose.yml docker-compose.yml
+docker-compose up -d $INPUT_SERVICE
+>>>>>>> Stashed changes
 sleep 20
